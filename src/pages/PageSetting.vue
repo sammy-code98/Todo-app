42d2a4858de663055dbc0ed6ee5d61bb2ac08440
sammy-code98 <sammysmart98@yahoo.com>
2021-09-19 22:20:34 +0100

    update

--- src/pages/PageSetting.vue
+++ src/pages/PageSetting.vue
@@ -1,55 +1,46 @@
 <template>
   <div>
     <div class="q-pa-md">
-      <q-list padding>
-        <q-item-label header class="text-h5 text-center">Settings</q-item-label>
-
+      <q-list bordered class="rounded-borders q-mb-md">
         <q-item tag="label" v-ripple>
           <q-item-section>
-            <q-item-label>Battery too low</q-item-label>
+            <q-item-label class="text-subtitle1 text-weight-medium text-grey"
+              >Dark Mode</q-item-label
+            >
           </q-item-section>
           <q-item-section side>
             <q-toggle color="blue" v-model="notif1" val="battery" />
           </q-item-section>
         </q-item>
+      </q-list>
 
-        <q-item tag="label" v-ripple>
-          <q-item-section>
-            <q-item-label>Friend request</q-item-label>
-            <q-item-label caption>Allow notification</q-item-label>
-          </q-item-section>
-          <q-item-section side top>
-            <q-toggle color="green" v-model="notif2" val="friend" />
-          </q-item-section>
-        </q-item>
-
-        <q-item tag="label" v-ripple>
-          <q-item-section>
-            <q-item-label>Picture uploaded</q-item-label>
-            <q-item-label caption
-              >Allow notification when uploading images</q-item-label
-            >
-          </q-item-section>
-          <q-item-section side top>
-            <q-toggle color="red" v-model="notif3" val="picture" />
-          </q-item-section>
-        </q-item>
+      <q-list bordered class="rounded-borders q-mb-md">
+        <q-expansion-item
+          label="About"
+          class="text-subtitle1 text-weight-medium text-grey"
+        >
+          <q-card>
+            <q-card-section>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem,
+              eius reprehenderit eos corrupti commodi magni quaerat ex numquam,
+              dolorum officiis modi facere maiores architecto suscipit iste
+              eveniet doloribus ullam aliquid.
+            </q-card-section>
+          </q-card>
+        </q-expansion-item>
       </q-list>
     </div>
   </div>
 </template>
 
 <script>
-export default{
-    data(){
-        return{
-        
-        notif1:false,
-        notif2: false,
-        notif3:false
-        }
-
-
-    }
-}
+export default {
+  data() {
+    return {
+      notif1: false,
+      notif2: false,
+      notif3: false
+    };
+  }
+};
 </script>
